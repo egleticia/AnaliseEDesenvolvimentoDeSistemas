43b22898d3a28a7cd287d135ef4f85bfdd955b5a
egleticia <leticiaegle@yahoo.com.br>
2023-03-20 21:00:17 -0300

    Recursividade

---
+++ Recursividade-Tabuada/Recursividade-Tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+
+Console.WriteLine("Escreva um número para sua tabuada: ");
+int t = int.Parse(Console.ReadLine());
+
+int i = 0;
+tabuada(t, i);
+
+
+
+static void tabuada (int n, int numero)
+{
+    if (numero > 10)
+    {
+        return;
+
+    }
+
+    int x = n * numero;
+    Console.WriteLine($"{n} * {numero} = {x}");
+
+    tabuada(n, numero+1);
+
+}
---
+++ Recursividade/Recursividade/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+
+
+
+contar(int.Parse(Console.ReadLine()));
+
+static void contar(int n)
+{
+    for (int i = n; i >= 0; i--)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+Console.WriteLine("Informe um número: ");
+int n = int.Parse(Console.ReadLine());
+
+int f = fatorial(n);
+Console.WriteLine("Fatorial é " + f);
+
+static int fatorial(int n)
+{
+    if (n == 1)
+        return 1;
+
+    return n * fatorial(n - 1);
+  
+}
